fd2431cb3a7ce4223b67ee501eb44f1a9d4233d5
Uliana Gorbolskaya <u.gorbolsky@gmail.com>
2016-06-01 16:20:02 -0400

    testing if it will load a simple page with just a heading

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ app = Flask(__name__)
 def main():
   return redirect('/index')
 
+
 @app.route('/index', methods=['GET'])
 def index():
   return render_template('index.html')
+
+
 @app.route('/index', methods=['POST'])
 def index2():
 	app.ticker=request.form['ticker']
@@ -23,23 +26,23 @@ def index2():
 
 @app.route('/plot_app', methods=['GET','POST'])
 def plot_app():
-	ticker=app.ticker
-	url='https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json'
-	json_obj= urllib2.urlopen(url)
-	data = json.load(json_obj)
+#	ticker=app.ticker
+#	url='https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json'
+#	json_obj= urllib2.urlopen(url)
+#	data = json.load(json_obj)
 
-	last_month=data['dataset']['data'][0:30]
-	date=[]
-	quotes=[]
-	for i in last_month:
-    		date.append(i[0])
-    		quotes.append(i[4])
+#	last_month=data['dataset']['data'][0:30]
+#	date=[]
+#	quotes=[]
+#	for i in last_month:
+#    		date.append(i[0])
+#    		quotes.append(i[4])
+#
+#	plotting_data=pd.DataFrame({'Quote':quotes}, index=date)
+#	plot = TimeSeries(plotting_data, title="Stock prices, previous 30 days", ylabel='Stock Price', xlabel='Date')
+#	script, div = components(plot)
+#	return render_template('plot.html', ticker=app.ticker,script=script, div=div)
+	return render_template('plot2.html')
 
-	plotting_data=pd.DataFrame({'Quote':quotes}, index=date)
-	plot = TimeSeries(plotting_data, title="Stock prices, previous 30 days", ylabel='Stock Price', xlabel='Date')
-	script, div = components(plot)
-	return render_template('plot.html', ticker=app.ticker,script=script, div=div)
-
-
-if __name__ == '__main__':
-  app.run(port=33507)
+#if __name__ == '__main__':
+#  app.run(port=33507)
