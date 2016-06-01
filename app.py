ca233c13f22284d02735fdf9bcd2c71620e96af9
Uliana Gorbolskaya <u.gorbolsky@gmail.com>
2016-06-01 18:04:12 -0400

    back to plot.html put script back in the header

--- app.py
+++ app.py
@@ -26,23 +26,22 @@ def index2():
 
 @app.route('/plot_app', methods=['GET','POST'])
 def plot_app():
-#	ticker=app.ticker
-#	url='https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json'
-#	json_obj= urllib2.urlopen(url)
-#	data = json.load(json_obj)
+	ticker=app.ticker
+	url='https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json'
+	json_obj= urllib2.urlopen(url)
+	data = json.load(json_obj)
 
-#	last_month=data['dataset']['data'][0:30]
-#	date=[]
-#	quotes=[]
-#	for i in last_month:
-#    		date.append(i[0])
-#    		quotes.append(i[4])
-#
-#	plotting_data=pd.DataFrame({'Quote':quotes}, index=date)
-#	plot = TimeSeries(plotting_data, title="Stock prices, previous 30 days", ylabel='Stock Price', xlabel='Date')
-#	script, div = components(plot)
-#	return render_template('plot.html', ticker=app.ticker,script=script, div=div)
-	return render_template('plot2.html')
+	last_month=data['dataset']['data'][0:30]
+	date=[]
+	quotes=[]
+	for i in last_month:
+    		date.append(i[0])
+    		quotes.append(i[4])
+
+	plotting_data=pd.DataFrame({'Quote':quotes}, index=date)
+	plot = TimeSeries(plotting_data, title="Stock prices, previous 30 days", ylabel='Stock Price', xlabel='Date')
+	script, div = components(plot)
+	return render_template('plot.html', ticker=app.ticker,script=script, div=div)
 
 #if __name__ == '__main__':
 #  app.run(port=33507)
