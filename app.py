17ef858d761978ec10e8a9618f479fe292129014
Uliana Gorbolskaya <u.gorbolsky@gmail.com>
2016-06-02 10:27:32 -0400

    last one didnt work, checking in ticker variable is being generated properly

--- app.py
+++ app.py
@@ -25,21 +25,21 @@ def index2():
 @app.route('/plot_app', methods=['GET','POST'])
 def plot_app():
 	tic=app.ticker
-	url='https://www.quandl.com/api/v3/datasets/WIKI/'+tic+'.json'
-	json_obj= urllib2.urlopen(url)
-	data = json.load(json_obj)
+	#url='https://www.quandl.com/api/v3/datasets/WIKI/'+tic+'.json'
+	#json_obj= urllib2.urlopen(url)
+	#data = json.load(json_obj)
 
-	last_month=data['dataset']['data'][0:30]
-	date=[]
-	quotes=[]
-	for i in last_month:
-    		date.append(i[0])
-    		quotes.append(i[4])
+	#last_month=data['dataset']['data'][0:30]
+	#date=[]
+	#quotes=[]
+	#for i in last_month:
+    	#	date.append(i[0])
+    	#	quotes.append(i[4])
 
 	#plotting_data=pd.DataFrame({'Quote':quotes}, index=date)
 	#plot = TimeSeries(plotting_data, title="Stock prices, previous 30 days", ylabel='Stock Price', xlabel='Date')
 	#script, div = components(plot)
 	#return render_template('plot.html', ticker=app.ticker,script=script, div=div)
-	return render_template('plot.html', ticker=tic, d=date, q=quotes)	
+	return render_template('plot.html', ticker=tic) #, d=date, q=quotes)	
 #if __name__ == '__main__':
 #  app.run(port=33507)
