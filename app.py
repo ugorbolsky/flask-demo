8c658d967ee6650cc350d950760655f3ddaa0cf3
Uliana Gorbolskaya <u.gorbolsky@gmail.com>
2016-06-02 13:38:18 -0400

    put all the variable text in app

--- app.py
+++ app.py
@@ -38,9 +38,16 @@ def plot_app():
     		quotes.append(i[4])
 
 	plotting_data=pd.DataFrame({'Quote':quotes}, index=date)
+	plotting_data=plotting_data.sort_index()
 	plot = TimeSeries(plotting_data, title="Stock prices, previous 30 days", ylabel='Stock Price', xlabel='Date')
 	app_script, app_div = components(plot)
-	return render_template('plot.html', ticker=tic, script=app_script, div=app_div)
+	app_head='<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">\
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">\
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>\
+		<link rel="stylesheet" href="https://cdn.pydata.org/bokeh/release/bokeh-0.11.1.min.css" type="text/css" />\
+                <script type="text/javascript" src="https://cdn.pydata.org/bokeh/release/bokeh-0.11.1.min.js"></script>'
+
+	return render_template('plot.html', head =app_head, ticker=tic, script=app_script, div=app_div)
 
 if __name__ == '__main__':
   app.run(port=33507)
